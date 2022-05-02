4c2a645c72da37b793133579457a6805889f4407
Yusuf Boyacı <85557228+yusufboyaci@users.noreply.github.com>
2022-05-02 13:27:16 +0300

    repository oluşturuldu

---
+++ DataAccess/Repositories/Concrete/EntityRepositories.cs
@@ -0,0 +1,68 @@
+﻿using Core.Enum;
+using DataAccess.Context;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Concrete
+{
+    public class ArabaRepository : Repository<Araba>
+    {
+        private readonly AppDbContext _context;
+        public ArabaRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+    public class GaleriRepository : Repository<Galeri>
+    {
+        private readonly AppDbContext _context;
+        public GaleriRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+    public class SiparisRepository : Repository<Siparis>
+    {
+        private readonly AppDbContext _context;
+        public SiparisRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public List<Siparis> OnaylanmamisSiparisleriListele()
+        {
+            return GetDefault(x => x.Status == Status.Active && x.OnaylandiMi == false).OrderByDescending(x => x.CreatedDate).ToList();
+        }
+        public int OnaylanmamisSiparislerinSayisiniGoster()
+        {
+            return GetDefault(x => x.Status == Status.Active && x.OnaylandiMi == false).ToList().Count;
+        }
+    }
+    public class SiparisDetayRepository : Repository<SiparisDetay>
+    {
+        private readonly AppDbContext _context;
+        public SiparisDetayRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+    public class MusteriRepository : Repository<Musteri>
+    {
+        private readonly AppDbContext _context;
+        public MusteriRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public Musteri MusteriKullaniciAdiniGetir(string kullaniciAdi)
+        {
+            return GetByDefault(x => x.KullaniciAdi == kullaniciAdi);
+        }
+        public bool KimlikKontrolEt(string kullaniciAdi, string sifre)
+        {
+            return Any(x => x.KullaniciAdi == kullaniciAdi && x.Password == sifre);
+        }
+    }
+}
