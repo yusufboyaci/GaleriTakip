70f837d64889682be13448c6eb9e4f7c5e693b3e
Yusuf Boyacı <85557228+yusufboyaci@users.noreply.github.com>
2022-06-19 13:46:30 +0300

    ---

---
+++ WebUI/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+{
+    public class LoginViewModel
+    {
+        [Display(Name ="Kullanıcı Adı")]
+        public string KullaniciAdi { get; set; }
+        [Display(Name = "Şifre")]
+        public string Sifre { get; set; }
+    }
+}
