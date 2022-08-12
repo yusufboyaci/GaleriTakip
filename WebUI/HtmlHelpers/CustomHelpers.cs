b2161bb2964e10bfcccdbf7c0e99d0c4e8d9e87a
yusuf.boyaci <yusuf.boyaci@agarthateknoloji.com.tr>
2022-08-13 01:33:43 +0300

    ---13082022 ---0133

---
+++ WebUI/HtmlHelpers/CustomHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace WebUI.HtmlHelpers
+{/// <summary>
+/// Extension method html helper için araştır.
+/// </summary>
+    public static class CustomHelpers
+    {
+        public static HtmlString TextBoxFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression, bool isReadOnly)
+        {
+            HtmlString html = default(HtmlString);
+            if (isReadOnly)
+            {
+                html = (HtmlString)HtmlHelperInputExtensions.TextBoxFor(htmlHelper, expression, new { @class = "readOnly", @readonly = "read-only" });
+            }
+            else
+            {
+                html = (HtmlString)HtmlHelperInputExtensions.TextBoxFor(htmlHelper, expression);
+            }
+            return html;
+        }
+        public static HtmlString yusuf<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression, bool isReadOnly)
+        {
+            HtmlString html = default(HtmlString);
+            if (isReadOnly)
+            {
+                html = (HtmlString)HtmlHelperInputExtensions.TextBoxFor(htmlHelper, expression, new { @class = "readOnly", @readonly = "read-only" });
+            }
+            else
+            {
+                html = (HtmlString)HtmlHelperInputExtensions.TextBoxFor(htmlHelper, expression);
+            }
+            return html;
+        }
+    }
+}
